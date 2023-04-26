582f220187ca69711a5bfb4587cf14cfee273909
readf0x <davis.a.forsythe@gmail.com>
2023-04-25 20:42:06 -0500

    finished search

--- src/components/SearchResults.vue
+++ src/components/SearchResults.vue
@@ -15,26 +15,100 @@ export default {
                 }
             }
         },
+        clamp(num, min, max) {
+            return Math.min(Math.max(num, min), max)
+        },
         searchIndex() {
-            for(const item of this.index.About) {
-                let element = document.createElement("p")
-                element.innerHTML = item
-                document.querySelector(".search-results").appendChild(element)
+            for(const file in this.index) {
+                this.results[file] = []
+                this.index[file].forEach(line => {
+                    // let resultIndex = line.search(new RegExp(`${this.query}`, "gi"))
+                    let splitLine = line.split(" ")
+                    let resultIndex = splitLine.findIndex((reg) => {
+                        // console.log(reg)
+                        return new RegExp(`${this.query}`, "gi").test(reg)
+                    })
+                    console.log(resultIndex)
+                    if(resultIndex > -1) {
+                        // this.results.push(line.slice(this.clamp(resultIndex - 40, 0, line.length), this.clamp(resultIndex + this.query.length + 40, 0, line.length)))
+                        this.results[file].push(
+                            splitLine.slice(
+                                this.clamp(resultIndex - 6, 0, splitLine.length),
+                                this.clamp(resultIndex + 8, 0, splitLine.length)
+                            ).join(" ")
+                        )
+                    }
+                })
+                if(this.results[file].length == 0) {
+                    delete this.results[file]
+                }
+            }
+        },
+        displayResults() {
+            let div =  document.querySelector(".result-list")
+            for(const file in this.results) {
+                let title = document.createElement("a")
+                title.href = "/" + file.toLowerCase() + "/"
+                title.innerHTML = file + ":"
+                title.classList.add("result-title")
+                div.appendChild(title)
+                this.results[file].forEach(result => {
+                    let itemWrapper = document.createElement("div")
+                    itemWrapper.classList.add("item-wrapper")
+                    let item = document.createElement("a")
+                    item.href = "/" + file.toLowerCase() + "/#:~:text=" + result.split(":")[0].replace(/,/g, "%2C").replace(/'/g, "%27").replace(/"/g, "%22")
+                    item.innerHTML = result + "..."
+                    item.classList.add("result-item")
+                    item.classList.add("ps-3")
+                    itemWrapper.appendChild(item)
+                    div.appendChild(itemWrapper)
+                })
             }
         }
     },
     data() {
         return {
             query: this.getQueryVariable("q"),
-            index: json
+            index: json,
+            results: {}
         }
+    },
+    mounted() {
+        this.searchIndex()
+        this.displayResults()
     }
 }
 </script>
 
 <template>
     <div class="search-results">
-        <button @click="searchIndex()">fuckery</button>
-        <p>{{ query }}</p>
+        <h2>Search results for: "{{ query }}"</h2>
+        <!-- <p>{{ results }}</p> -->
+        <div class='result-list'>
+
+        </div>
     </div>
 </template>
+
+<style lang="scss">
+@import '../node_modules/nord/src/sass/nord.scss';
+
+.result-title {
+    font-weight: bold;
+    font-size: 24px;
+    text-decoration: none;
+}
+.item-wrapper {
+    width: 50%;
+    border-top: 1px solid $nord3;
+    .result-item {
+        display: block;
+        color: inherit;
+        text-decoration: none;
+        width: fit-content;
+        &:hover {
+            color: $nord4;
+        }
+    }
+}
+</style>
