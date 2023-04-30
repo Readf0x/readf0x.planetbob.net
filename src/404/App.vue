7a239f1a482c431aa8ab20b2b387dbf05c710814
readf0x <davis.a.forsythe@gmail.com>
2023-04-30 17:22:40 -0500

    added 404 page

---
+++ src/404/App.vue
@@ -0,0 +1,26 @@
+<script setup>
+import Marked from '../components/Marked.vue'
+import MobileNav from '../components/MobileNavBar.vue'
+import Navbar from '../components/NavBar.vue'
+import PageFooter from '../components/PageFooter.vue'
+import MobileDetect from 'mobile-detect/mobile-detect'
+</script>
+
+<script>
+export default {
+    data() {
+        return {
+            md: new MobileDetect(window.navigator.userAgent)
+        }
+    }
+}
+</script>
+
+<template>
+    <navbar v-if="md.mobile() == null" />
+    <mobile-nav v-if="md.mobile() != null" />
+    <div class="content container-md">
+        <Marked parse="/markdown/404.md" />
+    </div>
+    <PageFooter />
+</template>
